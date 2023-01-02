f2a22b2ab28a32299edc5d54043fca10c4abb57b
BusterSR <121201452+BusterSR@users.noreply.github.com>
2023-01-01 19:13:56 -0800

    Add files via upload
    files added

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var number = 10
+document.getElementById('numberInput').addEventListener('change', function() {
+    number = document.getElementById('numberInput').value;
+  });
+
+function setup() {
+  createCanvas(windowWidth - 50, windowHeight - 30);
+  noStroke()
+  background('white');
+  frameRate(600)
+  fill('black')
+}
+
+function draw() {
+  if (mouseIsPressed){
+    circle(mouseX, mouseY, number)
+  }
+}
+
+
+function mousePressed(){
+  circle(mouseX, mouseY, number)
+}
+
+while (1 < 2){
+  if (mouseIsPressed){
+    circle(mouseX, mouseY, number)
+  }
+}
