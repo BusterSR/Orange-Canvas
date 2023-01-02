432053de9ca179f6780522c250b842b74940524a
BusterSR <121201452+BusterSR@users.noreply.github.com>
2023-01-02 10:13:45 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,12 +4,15 @@ var prevY
 document.getElementById('numberInput').addEventListener('change', function() {
     number = document.getElementById('numberInput').value;
   });
+document.getElementById('colors').addEventListener('change', function() {
+    theColor = document.getElementById('colors').value;
+  });
 document.getElementById('clear').addEventListener('click', function() {
   clear()
   createCanvas(windowWidth - 50, windowHeight - 30);
   background(255, 254, 252);
   frameRate(600)
-  fill('black')
+  fill(theColor)
 });
 
 document.getElementById('save').addEventListener('click', function() {
@@ -20,11 +23,12 @@ function setup() {
   createCanvas(windowWidth - 50, windowHeight - 30);
   background(255, 254, 252);
   frameRate(600)
-  fill('black')
+  fill(theColor)
 }
 
 function draw() {
   strokeWeight(number);
+  fill(theColor)
   if (mouseIsPressed){
     line(prevX, prevY, mouseX, mouseY);
   }
